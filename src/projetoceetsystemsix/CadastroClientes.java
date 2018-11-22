e2ae056d9c17a2b7af9aa25ecd5266d3ffc04eae
Kaio Rosa <kaiosrosa@gmail.com>
2018-11-22 21:07:39 -0200

    Revert "novo"
    This reverts commit 9867f7fa21c4395ab4a425f4b82cc661dcc30434.

--- src/projetoceetsystemsix/CadastroClientes.java
+++ src/projetoceetsystemsix/CadastroClientes.java
@@ -283,7 +283,7 @@ public class CadastroClientes extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
