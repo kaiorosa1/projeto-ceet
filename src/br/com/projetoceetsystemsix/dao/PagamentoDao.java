5ce1779b8b7a4a8f3834f01b734b22869d984a33
kaiorosa1 <kaiosrosa@gmail.com>
2018-12-11 16:29:00 -0200

    template PagamentoDao

--- src/br/com/projetoceetsystemsix/dao/PagamentoDao.java
+++ src/br/com/projetoceetsystemsix/dao/PagamentoDao.java
@@ -5,10 +5,26 @@
  */
 package br.com.projetoceetsystemsix.dao;
 
+import br.com.projetoceetsystemsix.model.Pagamento;
+import java.util.List;
+
 /**
  *
  * @author user
  */
 public class PagamentoDao {
-    
+    public void salvarPagamento(Pagamento pag){
+        //conectar com o banco de dados aqui
+        
+    }
+    public List<Pagamento> BuscarPagamento(int idPagamento){
+        //falta verificar se a chave na tabela eh um int 
+        return null;
+    }
+    public List<Pagamento> BuscarAllPagamento(){
+        return null;
+    }
+    public void alterarPagamento(Pagamento pag){
+        //alterar um fornecedor existente existente
+    }
 }
