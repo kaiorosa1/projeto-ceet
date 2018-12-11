7655e37a7a54ebf7e1f2696ef94eadc8efffc350
kaiorosa1 <kaiosrosa@gmail.com>
2018-12-11 16:33:36 -0200

    classe de ligacao com o banco
    <<<incompleta>>>

---
+++ src/br/com/projetoceetsystemsix/connection/DBConnection.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projetoceetsystemsix.connection;
+
+/**
+ *
+ * @author user
+ */
+public class DBConnection {
+    //aqui sera feita per say a ligacao com o banco de dados
+}
