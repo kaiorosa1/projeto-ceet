2858cf285e4e49ff8ba13c6d59ea1c397f82d091
kaiorosa1 <kaiosrosa@gmail.com>
2018-12-12 17:43:09 -0200

    update ClienteDao
    salvando no banco mas incompleto

--- src/br/com/projetoceetsystemsix/dao/ClienteDao.java
+++ src/br/com/projetoceetsystemsix/dao/ClienteDao.java
@@ -5,7 +5,11 @@
  */
 package br.com.projetoceetsystemsix.dao;
 
+import br.com.projetoceetsystemsix.connection.DBConnection;
 import br.com.projetoceetsystemsix.model.Cliente;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 /**
@@ -14,8 +18,35 @@ import java.util.List;
  */
 public class ClienteDao {
     public void salvarCliente(Cliente cliente){
-        //conectar com o banco de dados aqui
-        
+        //falta some arrengents
+        String sql = "INSERT INTO cliente(nome, endereco, bairro, clienteDesde, telefoneCelular, email) VALUES(?,?,?,?,?,?)";
+        Connection conn = null;
+        PreparedStatement pstm = null;
+        //difereciar se eh fisico ou juridico antes de inserir
+        try {
+            conn = DBConnection.conectarMysql();
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, cliente.getNome());
+            pstm.setString(2, cliente.getEndereco());  
+            pstm.setString(3, cliente.getBairro()); 
+            pstm.setDate(4, (Date) cliente.getClienteDesde());
+            pstm.setInt(5, cliente.getTelefoneCelular());
+            pstm.setString(6, cliente.getEmail());
+            
+            pstm.setString(4, sql);
+            pstm.execute();
+            
+        } catch (Exception e) {
+            
+            e.getMessage();
+        }finally{
+            try {
+                if(pstm!=null){
+                    pstm.close();
+                }
+            } catch (Exception e) {
+            }
+        }
     }
     public List<Cliente> BuscarCliente(int idCliente){
         //falta verificar se a chave na tabela eh um int 
